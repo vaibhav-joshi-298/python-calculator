c7233c5dbfa1383a5498e7b3bf3de4465e18fce2
vaibhav-joshi-298 <vj2998743@gmail.com>
2026-05-17 16:52:04 +0530

    Raj added test cases in add and subtract

--- addition.py
+++ addition.py
@@ -3,3 +3,7 @@ def addition(a, b):
     return a + b
 
 print(addition(5, 3))  
+
+# Raj is adding test cases here
+print(addition(10, 20))
+print(addition(-5, 15))
--- subtraction.py
+++ subtraction.py
@@ -4,3 +4,7 @@ def subtraction(a, b):
 
 # Test case
 print(subtraction(5,3))
+
+# Raj is adding test cases here
+print(subtraction(10, 4))
+print(subtraction(-5, -10))
